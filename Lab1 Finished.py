f4c5c161acf1db862b441bca82ac1e8d3140944c
ggangwer01 <ggangwer01@saintmarys.edu>
2024-08-28 16:58:14 -0400

    Add files via upload

---
+++ Lab1 Finished.py
@@ -0,0 +1,118 @@
+Python 3.12.5 (v3.12.5:ff3bc82f7c9, Aug  7 2024, 05:32:06) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+#the authors' name are: Gwyn and Colleen
+("Hello, world")
+'Hello, world'
+("Welcome to CPSC 207")
+'Welcome to CPSC 207'
+("Hello/nWorld")
+'Hello/nWorld'
+("Hello\nWorld")
+'Hello\nWorld'
+'Hello\nWorld'
+'Hello\nWorld'
+("Hello Gwyn!")
+'Hello Gwyn!'
+2+2
+4
+3.14+0.1
+3.24
+3.14-0.14
+3.0
+print("Hello, world")
+Hello, world
+print("Welcome to CPSC 207")
+Welcome to CPSC 207
+
+=============== RESTART: /Users/gwynethgangwer/Documents/Lab1.py ===============
+Welcome to CPSC 207!
+# the authors' name are: Colleen and Gwyn
+print("Hello, world")
+Hello, world
+print("Welcome to CPSC 207")
+Welcome to CPSC 207
+print("Hello\nWorld")
+Hello
+World
+print("How\nare\nyou?")
+How
+are
+you?
+print("Today","is","Wednesday")
+Today is Wednesday
+print("Today"+ "is"+"Wednesday")
+TodayisWednesday
+print("a\nb\nc")
+a
+b
+c
+print("Hello" + "World)
+      
+SyntaxError: unterminated string literal (detected at line 1)
+print("Hello" + "World")
+      
+HelloWorld
+print("Hello\n" + "\nWorld!")
+      
+Hello
+
+World!
+print("123\n45\n6")
+      
+123
+45
+6
+print("1+2*3")
+      
+1+2*3
+1+2*3
+      
+7
+(1+2)*3
+      
+9
+7/3
+      
+2.3333333333333335
+7//3
+      
+2
+>>> 7%3
+...       
+1
+>>> 22/4
+...       
+5.5
+>>> 22//4
+...       
+5
+>>> 22%4
+...       
+2
+>>> 3**2
+...       
+9
+>>> 2**3
+...       
+8
+>>> (4+3)//2
+...       
+3
+>>> 6%2
+...       
+0
+>>> 1+(2*3**4)
+...       
+163
+>>> print("2+2=
+...       
+SyntaxError: unterminated string literal (detected at line 1)
+>>> print ("2+2=")
+...       
+2+2=
+>>> print("2+2=",2+2)
+...       
+2+2= 4
+>>> print("50 pounds is",50*0.453592,"Kilograms")
+...       
+50 pounds is 22.6796 Kilograms
